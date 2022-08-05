2ea78aa95b557f96e9e28f12162cadf54111f552
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2022-08-05 16:20:34 -0300

    feat: userController, userService; function: login

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -14,7 +14,15 @@ const getByEmailOrName = async (req, _res, next) => {
   next();
 };
 
+const login = async (req, _res, next) => {
+  const { email, hash: password } = req.body;
+  const loggedUser = userService.login(email, password);
+  const token = generateJWT(loggedUser);
+  return res.status(200).json({ ...loggedUser, token });
+};
+
 module.exports = {
   create,
   getByEmailOrName,
+  login,
 };
--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,5 +1,6 @@
 const { user } = require('../database/models');
 const errorObject = require('../utils/errorObject');
+const md5 = require('md5');
 
 const create = async (name, email, password, role) => {
   const newUser = await user.create({ name, email, password, role });
@@ -15,6 +16,15 @@ const getByEmailOrName = async (name, email) => {
     ],
   } });
   if (userByEmailOrName) throw errorObject(409, 'User already registered');
+};
+
+const login = async (email, password) => {
+  const loggedUser = await user.findOne({ where: { email }});
+  if (!loggedUser || password !== loggedUser.password) {
+    throw errorObject(400, 'Invalid email or password');
+  }
+  const { password: noPassword, ...userWithoutPassword } = loggedUser.dataValues;
+  return userWithoutPassword;
 };
 
 module.exports = {
