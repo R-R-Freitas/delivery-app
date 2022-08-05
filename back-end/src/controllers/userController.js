68adc9626eb2b71e6685df438d1de0d47c62396d
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2022-08-05 09:35:28 -0300

    feat: user controller create

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -3,7 +3,7 @@ const userService = require('../services/userService');
 const create = async (req, res, next) => {
   const { name, email, password, role } = req.body;
   const user = await userService.create(name, email, password, role);
-  return res.status(200).json(user);
+  return res.status(201).json(user);
 };
 
 module.exports = {
