0b229d593a6262d3426f336348f01973e0d279bb
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2022-08-05 10:09:13 -0300

    feat: access control

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,7 +3,16 @@ const register = require('../routes/register');
 
 const app = express();
 
+const accessControl = (_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+};
+
 app.use(express.json());
+app.use(accessControl);
+
 app.use('/register', register);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
