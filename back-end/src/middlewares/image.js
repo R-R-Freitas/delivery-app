543c3acd81f304451774a0d1378694fc56f3c4ca
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2022-08-06 18:48:34 -0300

    fix: lint

--- back-end/src/middlewares/image.js
+++ back-end/src/middlewares/image.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const getImage = async(req, res, _next) => {
+const getImage = async (req, res, _next) => {
   const { filename } = req.params;
   const options = { root: path.join(__dirname, '../../images') };
   res.sendFile(filename, options);
