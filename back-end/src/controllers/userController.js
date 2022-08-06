53dc0cde15c96350d01037bcefecfc51720b1fa3
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2022-08-06 20:34:21 -0300

    fix: userController login

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -19,7 +19,7 @@ const login = async (req, res, _next) => {
   const { email, hash: password } = req.body;
   const loggedUser = await userService.login(email, password);
   const token = generateJWT(loggedUser);
-  const { id, userWithoutId } = loggedUser;
+  const { id, ...userWithoutId } = loggedUser;
   return res.status(200).json({ ...userWithoutId, token });
 };
 
