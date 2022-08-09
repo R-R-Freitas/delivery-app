09d964d19944d0cef2cd57e8c2c81ff3d80ff189
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2022-08-08 22:21:24 -0300

    feat: saleService findByUserId

--- back-end/src/services/saleService.js
+++ back-end/src/services/saleService.js
@@ -1,4 +1,4 @@
-const { sale } = require('../database/models');
+const { sale, user, product } = require('../database/models');
 
 const create = async (newSale) => {
   const { totalPrice, deliveryAddress, deliveryNumber, sellerId, userId } = newSale; 
@@ -19,6 +19,28 @@ const create = async (newSale) => {
   return id;
 };
 
+const findByUserId = async (userId) => {
+  const sales = await sale.findAll({
+    where: { userId },
+    include: [
+      { model: user, as: 'user', attributes: { exclude: ['password'] } },
+      { model: user, as: 'seller', attributes: { exclude: ['password'] } },
+      { model: product, as: 'products', through: { attributes: ['quantity'] } },
+    ],
+  });
+  const plainSales = sales.map((aSale) => aSale.get({ plain: true }));
+  const salesByCustomer = plainSales.map((aSale) => {
+    const { products, ...thisSale } = aSale;
+    const formatedProducts = products.map((aProduct) => {
+      const { salesProduct, ...bProduct } = aProduct;
+      return { quantity: salesProduct.quantity, ...bProduct };
+    });
+    return { products: formatedProducts, ...thisSale };
+  });
+  return salesByCustomer;
+};
+
 module.exports = {
   create,
+  findByUserId,
 };
