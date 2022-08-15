5f4e4e13d2d06954dd3e8f7e61034167a47b7355
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2022-08-15 18:17:54 -0300

    feat: CardUser component

---
+++ front-end/src/components/CardUser.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CardUser({ id, name, email, role, remove }) {
+  return (
+    <div>
+      <p
+        data-testid={ `admin_manage__element-user-table-item-number-${id}` }
+      >
+        {id}
+      </p>
+      <p>
+        R$:
+        {' '}
+        <span
+          data-testid={ `admin_manage__element-user-table-name-${id}` }
+        >
+          {name}
+        </span>
+      </p>
+      <p
+        data-testid={ `admin_manage__element-user-table-email-${id}` }
+      >
+        {email}
+      </p>
+      <p
+        data-testid={ `admin_manage__element-user-table-role-${id}` }
+      >
+        {role}
+      </p>
+      <button
+        data-testid={ `admin_manage__element-user-table-remove-${id}` }
+        type="button"
+        onClick={ () => remove(id) }
+      >
+        Excluir
+      </button>
+    </div>
+  );
+}
+
+CardUser.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+  remove: PropTypes.func.isRequired,
+};
+
+export default CardUser;
